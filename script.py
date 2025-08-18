49018b47e9fb878476fdce089bde80fb2c5bf5f7
Nick536363 <3383371nik@gmail.com>
2025-08-18 10:36:39 +0200

    Добавил удаление замечаний

--- script.py
+++ script.py
@@ -1,6 +1,11 @@
-from datacenter.models import Schoolkid, Mark
+from datacenter.models import Schoolkid, Mark, Chastisement, Lesson
 
 
 def fix_marks(schoolkid: Schoolkid):
     bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2,3])
-    bad_marks.update(points=5)    
+    bad_marks.update(points=5)
+
+
+def remove_chastisements(schoolkid: Schoolkid):
+    chasitsements = Chastisement.objects.filter(schoolkid=schoolkid)
+    chasitsements.delete()
