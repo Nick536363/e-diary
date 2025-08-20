8dfc1f4529968a5835e40d4b65eb3b57f3c18b82
Nick536363 <3383371nik@gmail.com>
2025-08-20 09:11:47 +0200

    Добавил функцию получения ученика по имени

--- script.py
+++ script.py
@@ -1,11 +1,16 @@
-from datacenter.models import Schoolkid, Mark, Chastisement, Lesson
+from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Subject
+
+def get_pupil_by_name(name: str):
+    return Schoolkid.objects.get(full_name__contains=name)
 
 
-def fix_marks(schoolkid: Schoolkid):
-    bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2,3])
+def fix_marks(schoolkid: str):
+    pupil = get_pupil_by_name(schoolkid)
+    bad_marks = Mark.objects.filter(schoolkid=pupil, points__in=[2,3])
     bad_marks.update(points=5)
 
 
-def remove_chastisements(schoolkid: Schoolkid):
-    chasitsements = Chastisement.objects.filter(schoolkid=schoolkid)
+def remove_chastisements(schoolkid: str):
+    pupil = get_pupil_by_name(schoolkid)
+    chasitsements = Chastisement.objects.filter(schoolkid=name)
     chasitsements.delete()
